3608c4b8225a2ca9e3f21b75b834f3ffeb986db5
donboyd5 <donboyd5@gmail.com>
2015-05-02 07:37:17 -0400

    Update doc

--- R/ppdvars.r
+++ R/ppdvars.r
@@ -5,7 +5,7 @@
 #' @source Center for State and Local Government Excellence (SLGE),\cr
 #'         National Association of State Retirement Administrators (NASRA), and\cr
 #'         Center for Retirement Research at Boston College\cr\cr
-#'         http://publicplansdata.org/public-plans-database/download-full-data-set/\cr
+#'         Link: http://publicplansdata.org/public-plans-database/download-full-data-set/\cr
 #' @format Data frame with 1 row per variable.
 #' @examples
 #'   ppdvars
