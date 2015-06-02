ac2d40646b79bc84d59a8cc83a4ad597c868effe
donboyd5 <donboyd5@gmail.com>
2015-06-02 09:03:58 -0400

    Updated ppd

--- data-raw/ppd/get_and_save_ppd.r
+++ data-raw/ppd/get_and_save_ppd.r
@@ -10,23 +10,19 @@
 #   http://publicplansdata.org/public-plans-database/documentation/
 
 #****************************************************************************************************
-#
 #                Load packages ####
-#
 #****************************************************************************************************
-library(btools)
-library(dplyr)
-library(lubridate)
-library(readr)
-library(readxl)
-library(stringr)
-library(tidyr)
+library("btools")
+library("dplyr")
+library("lubridate")
+library("readr")
+library("readxl")
+library("stringr")
+library("tidyr")
 
 
 #****************************************************************************************************
-#
 #                Define directories, filenames, etc. ####
-#
 #****************************************************************************************************
 
 ppd.dir <- "./data-raw/ppd/"
@@ -49,9 +45,7 @@ vlist.localfn <- paste0(downloaddate, "_", vlist.webfn)
 
 
 #****************************************************************************************************
-#
 #                Download and save raw data, with download date prefixed to file names ####
-#
 #****************************************************************************************************
 
 download.file(ppd.u, paste0(ppd.dir, ppd.localfn), mode="wb")
@@ -59,9 +53,7 @@ download.file(vlist.u, paste0(ppd.dir, vlist.localfn), mode="wb")
 
 
 #****************************************************************************************************
-#
 #                Clean variable info slightly, then save in package data directory ####
-#
 #****************************************************************************************************
 df <- read_excel(paste0(ppd.dir, vlist.localfn))
 glimpse(df)
@@ -71,9 +63,7 @@ devtools::use_data(ppdvars, overwrite=TRUE)
 
 
 #****************************************************************************************************
-#
 #                Clean ppd data slightly, then save in package data directory ####
-#
 #****************************************************************************************************
 # read all data as character and then convert; this appears to work better than letting read_csv use its defaults
 ncols <- ncol(read_csv(paste0(ppd.dir, ppd.localfn), n_max=1)) # determine # columns
@@ -89,14 +79,16 @@ df3 <- df2 %>% mutate_each(funs(as.Date(dmy(.))), fye, ActRptDate, ActValDate_GA
 names(df2)
 # get character version of dates
 cdates <- df2 %>% select(ppd_id, fy, fye, ActRptDate, ActValDate_GASBAssumptions, ActValDate_GASBSchedules) %>%
-  gather(variable, cdate, -ppd_id, -fy) %>%
+  mutate(row=row_number()) %>%
+  gather(variable, cdate, -ppd_id, -fy, -row) %>%
   filter(!is.na(cdate))
 # get date version of dates
 ddates <- df3 %>% select(ppd_id, fy, fye, ActRptDate, ActValDate_GASBAssumptions, ActValDate_GASBSchedules) %>%
-  gather(variable, ddate, -ppd_id, -fy) %>%
+  mutate(row=row_number()) %>%
+  gather(variable, ddate, -ppd_id, -fy, -row) %>%
   filter(!is.na(ddate))
 comp <- left_join(cdates, ddates) %>% filter(is.na(ddate), cdate!="")
-comp
+comp %>% arrange(row, variable)
 # this all looks like bad data and we probably need to delete the records
 count(df2, is.na(ppd_id))
 df2 %>% filter(is.na(ppd_id))
