7b0f7b60bbbcdb8033ca595096583042f8ebbbc5
donboyd5 <donboyd5@gmail.com>
2018-02-28 07:19:32 -0500

    Updated BEA and FRB pensions data

--- data-raw/StatesBEAFRB/beafrbstates.r
+++ data-raw/StatesBEAFRB/beafrbstates.r
@@ -1,4 +1,4 @@
-# 10/9/2017
+# 2/28/2018
 
 
 #****************************************************************************************************
@@ -36,13 +36,23 @@ library("bdata")
 #                Get FRB data  - last year 2014 as of 10/9/2017 ####
 #****************************************************************************************************
 # https://www.federalreserve.gov/apps/fof/efa/efa-project-state-local-government-defined-benefit-pension-plans.htm
+# Update (2/28/2018), now in the dataviz area:
+# https://www.federalreserve.gov/releases/z1/dataviz/pension/index.html
 # The FRB dollar data are in $ millions
 
-frb.states.url <- "https://www.federalreserve.gov/apps/fof/efa/files/efa-state-pension-tables-annual-historical.csv"
-frb.states.put <- "./data-raw/StatesBEAFRB/efa-state-pension-tables-annual-historical.csv"
+# https://www.federalreserve.gov/releases/z1/dataviz/download/zips/efa-state-pension-tables-annual-historical.zip
+
+# frb.states.url <- "https://www.federalreserve.gov/apps/fof/efa/files/efa-state-pension-tables-annual-historical.csv"
+# frb.states.put <- "./data-raw/StatesBEAFRB/efa-state-pension-tables-annual-historical.csv"
+
+frb.states.url <- "https://www.federalreserve.gov/releases/z1/dataviz/download/zips/efa-state-pension-tables-annual-historical.zip"
+frb.states.put <- "./data-raw/StatesBEAFRB/efa-state-pension-tables-annual-historical.zip"
 download.file(frb.states.url, frb.states.put, mode="wb")
 
-df.frb <- read_csv(frb.states.put)
+unzip(frb.states.put, exdir="./data-raw/StatesBEAFRB", overwrite = TRUE)
+csv.name <- "./data-raw/StatesBEAFRB/efa-state-pension-tables-annual-historical.csv"
+
+df.frb <- read_csv(csv.name)
 glimpse(df.frb)
 vnames <- c("year", "stname", "assets", "liabilities", "ufl", "fr", "ufl.gdp", "ufl.staterev")
 vdesc <- names(df.frb)
@@ -51,29 +61,32 @@ statepen.frb <- df.frb %>%
   setNames(vnames) %>%
   mutate(stabbr=stcodes$stabbr[match(stname, stcodes$stname)]) %>%
   gather(vname, value, -stabbr, -stname, -year) %>%
-  mutate(vdesc=vdesc[match(vname, vnames)]) %>%
+  mutate(vdesc=vdesc[match(vname, vnames)],
+         value=as.numeric(value)) %>%
   select(stabbr, vname, vdesc, year, value)
 glimpse(statepen.frb)
 count(statepen.frb, stabbr)
 count(statepen.frb, year)
 count(statepen.frb, vname, vdesc)
 
-cmt <- "FRB Enhanced Accounts State Pension Tables, $ Millions, FRB updated 2017-08-31. See package documentation for discount rates."
+statepen.frb %>% filter(is.na(value)) # ufl.staterev is missing in 2003, except for U.S.
+
+cmt <- "FRB Enhanced Accounts State Pension Tables, $ Millions, FRB updated 2018-01-11. See package documentation for discount rates."
 comment(statepen.frb) <- cmt
 devtools::use_data(statepen.frb, overwrite=TRUE)
 
 statepen.frb %>% filter(stabbr %in% c("US", "PA"), vname=="fr") %>%
-  ggplot(aes(year, value, colour=stabbr)) + geom_line() + geom_hline(yintercept = 100)
+  ggplot(aes(year, value, colour=stabbr)) + geom_line() + geom_point() + geom_hline(yintercept = 100)
 
 statepen.frb %>% filter(stabbr %in% c("US", "PA"), vname=="ufl.staterev") %>%
-  ggplot(aes(year, value, colour=stabbr)) + geom_line() + geom_hline(yintercept = 0) + geom_point()
+  ggplot(aes(year, value, colour=stabbr)) + geom_line() + geom_point() + geom_hline(yintercept = 0) + geom_point()
 
 statepen.frb %>% filter(stabbr %in% c("US", "PA", "MN"), vname=="ufl.gdp") %>%
   ggplot(aes(year, value, colour=stabbr)) + geom_line() + geom_hline(yintercept = 0) + geom_point()
 
 
 #****************************************************************************************************
-#                Get BEA data - last year 2015 as of 10/9/2017 ####
+#                Get BEA data - last year 2015 as of 1/26/2018 ####
 #****************************************************************************************************
 # BEA data is in $ Thousands but I convert to $ Millions
 # Landing page (look to the right for pensions): https://www.bea.gov/regional/
@@ -108,28 +121,28 @@ count(statepen.bea, vname)
 count(statepen.bea, year)
 count(statepen.bea, stabbr) # includes US and DC
 
-cmt <- "BEA State Pension Estimates Updated by BEA 2017-09-26. See package documentation for discount rates."
+cmt <- "BEA State Pension Estimates Updated by BEA 2018-01-26. See package documentation for discount rates."
 comment(statepen.bea) <- cmt
 devtools::use_data(statepen.bea, overwrite=TRUE)
 
 
 statepen.bea %>% filter(vname=="nc.er", stabbr=="CA") %>%
-  ggplot(aes(year, value)) + geom_line()
+  ggplot(aes(year, value)) + geom_line() + geom_point()
 
 statepen.bea %>% filter(vname=="nc.er", stabbr=="MN") %>%
-  ggplot(aes(year, value)) + geom_line()
+  ggplot(aes(year, value)) + geom_line() + geom_point()
 
 statepen.bea %>% filter(vname=="nc.er", stabbr %in% c("CA", "IL", "MN", "NY", "US")) %>%
   group_by(stabbr) %>%
   arrange(year) %>%
   mutate(ivalue=value / value[year==2005] * 100) %>%
-  ggplot(aes(year, ivalue, colour=stabbr)) + geom_line() + geom_hline(yintercept = 100)
+  ggplot(aes(year, ivalue, colour=stabbr)) + geom_line() + geom_point() + geom_hline(yintercept = 100)
 
 
 #****************************************************************************************************
 #                Compare liabilities, both sources ####
 #****************************************************************************************************
-# Good - the liabilities match
+# Good - the liabilities match (extremely closely)
 count(statepen.frb, vname)
 count(statepen.bea, vname)
 
